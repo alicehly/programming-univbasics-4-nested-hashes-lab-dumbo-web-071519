90426125f939f0e6dbd49eabf35205113e1c0136
Alice Ly Ms <alicehly@gmail.com>
2019-07-09 22:07:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,8 +127,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-return programmer_hash[:yukihiro_matsumoto] = {
+programmer_hash[:yukihiro_matsumoto] = {
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
  }
+ 
+ return programmer_hash
 end
